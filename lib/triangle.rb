3522ef89d6695e4f221d29e20309671b2b68aa7b
Annia Forney <anniaforney@gmail.com>
2019-05-14 07:07:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,7 @@ class Triangle
       elsif @triangleside_1 + @triangleside_2 < @triangleside_3 || @triangleside_2 + @triangleside_3 < @triangleside_1 || @triangleside_1 + @triangleside_3 < @triangleside_2
         raise TriangleError
     end
+  end 
 
     def kind
       if @triangleside_1 == @triangleside_2 && @triangleside_2 == @triangleside_3
