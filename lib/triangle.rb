986145eb2dbf7c7a900e91a36583fa67a7774918
Annia Forney <anniaforney@gmail.com>
2019-05-14 06:33:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
       elsif @triangleside_1 != @triangleside_2 && @triangleside_2 != @triangleside_3
         :scalene
       end
-      if @triangle.size < 0 
+      if @triangle.size < 0
         raise TriangleError
     end
   class TriangleError < StandardError
