4c11d57760a20c362ed3b98dc87cffecdf3f7e1d
Annia Forney <anniaforney@gmail.com>
2019-05-14 07:23:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,8 +10,6 @@ class Triangle
         raise TriangleError
       elsif @triangleside_1 < 0 || @triangleside_2 < 0 || @triangleside_3 < 0
         raise TriangleError
-      elsif @triangleside_1 = @triangleside_2 + @triangleside_3 || @triangleside_2 = @triangleside_1 + @triangleside_3
-        raise TriangleError
       elsif @triangleside_1 + @triangleside_2 < @triangleside_3 || @triangleside_2 + @triangleside_3 < @triangleside_1 || @triangleside_1 + @triangleside_3 < @triangleside_2
         raise TriangleError
     end
