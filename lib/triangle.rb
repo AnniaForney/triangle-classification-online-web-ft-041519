18ad6806d5ba8bbbbd465c2ba4911b8fe1c711a7
Annia Forney <anniaforney@gmail.com>
2019-05-14 06:27:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_reader :triangleside_1, :triangleside_2, :triangleside_3
+
+    def initialize(triangleside_1, triangleside_2, triangleside_3)
+      @triangleside_1 = triangleside_1
+      @triangleside_2 = triangleside_2
+      @triangleside_3 = triangleside_3
+    end
+
+    def kind
+      if @triangleside_1 == @triangleside_2 && @triangleside_2 == @triangleside_3
+        :equilateral
+      elsif @triangleside_1 == @triangleside_2 || @triangleside_2 == @triangleside_3 || @triangleside_1 == @triangleside_3
+        :isosceles
+      elsif @triangleside_1 != @triangleside_2 && @triangleside_2 != @triangleside_3
+        :scalene
+      end
+    end
+  class TriangleError < StandardError
+  end
 end
