9b1d03de02dcdac6973f6455ed5e2eea187c5796
Annia Forney <anniaforney@gmail.com>
2019-05-14 06:48:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,8 +10,8 @@ class Triangle
       if @triangleside_1 == 0 || @triangleside_2 == 0 || @triangleside_3 == 0
         raise TriangleError
       elsif @triangleside_1 < 0 || @triangleside_2 < 0 || @triangleside_3 < 0
-        raise TriangleError 
-      end 
+        raise TriangleError
+      end
     end 
     def kind
       if @triangleside_1 == @triangleside_2 && @triangleside_2 == @triangleside_3
